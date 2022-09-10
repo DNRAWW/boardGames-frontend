0001e365dadeeef082d5857ff02728cf7d783f5c
Kostya_Moguchev <rjcnz222@gmail.com>
2022-09-10 18:17:19 +0300

    promotion close button

--- app/components/chess/promotionComponent.tsx
+++ app/components/chess/promotionComponent.tsx
@@ -10,8 +10,6 @@ interface PromotionProps {
   eventEmitter: TypedEmitter<PromotionEvents>;
 }
 
-// TODO: Button close, background
-
 export default function PromotionComponent(props: PromotionProps) {
   const pieces = [Pieces.BISHOP, Pieces.KNIGHT, Pieces.QUEEN, Pieces.ROOK];
   const piecesToRender: JSX.Element[] = [];
@@ -20,10 +18,10 @@ export default function PromotionComponent(props: PromotionProps) {
     piecesToRender.push(
       <div
         className={
-          "px-4  " +
+          "px-4 cursor-pointer " +
           (piece !== Pieces.ROOK
             ? "border-x-0 border-y-0 border-r-2 border-solid"
-            : "")
+            : "pr-0")
         }
         onClick={() => {
           props.eventEmitter.emit("promote", props.from, props.to, piece);
@@ -37,10 +35,10 @@ export default function PromotionComponent(props: PromotionProps) {
   }
 
   return (
-    <div className="flex justify-center bg-emerald-300 rounded-2xl">
+    <div className="flex bg-emerald-300 rounded-2xl">
       <div className="flex">{piecesToRender}</div>
       <button
-        className="block h-5 border-none rounded-md mt-1 mr-4"
+        className="h-5 mt-1 mr-1 rounded-md border-none"
         onClick={() => {
           props.eventEmitter.emit("closePromotion");
         }}
